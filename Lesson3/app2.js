375650e3d9277e38ff36db153a386b03ae73fd16
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-09-29 19:22:56 +0900

    [Finish] JavaScript Lesson3-2

---
+++ Lesson3/app2.js
@@ -0,0 +1,10 @@
+// Lesson3-2
+
+// 入力ダイアログの表示
+// var promptStr = prompt('何か好きな文字を入力してください。');
+// 入力された文字列は"prompt関数"の戻り値として"変数promptStr"に格納される。
+// alert(promptStr);
+
+var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+alert('あなたの選んだ手は' + user_hand + 'です。');
