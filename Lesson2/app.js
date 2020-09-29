a424e2c2f8dc47ed0e058256032ec047d7d44ce0
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-09-29 19:05:18 +0900

    [Finish] JavaScript Lesson2-1

--- Lesson2/app.js
+++ Lesson2/app.js
@@ -1,3 +1,12 @@
+// var 変数名 = 内容 ;
+var str1 = 'Hello';
+var str2 = 'World!!';
+alert(str1 + str2);
+
+// ↓ 実行結果はすべて同じ
 // alert('Hello World');
-var hello = 'Hello World';
-alert(hello);
+
+// var hello = 'Hello World';
+// alert(hello);
+
+// alert('Hello' + 'World');
