da1d33f86826747e69c7445d765eaceb4a6fc3fe
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-09-29 19:15:52 +0900

    [Finish] JavaScript Lesson3-1

---
+++ Lesson3/app.js
@@ -0,0 +1,19 @@
+// Lesson3-1
+
+// メイン部分
+var alertString;
+alertString = addString("WebCamp");
+
+// 作成した関数を呼び出す
+alert(alertString);
+
+// 作成した関数
+function addString(strA){
+  var addStr = "Hello " + strA;
+  return addStr;
+}
+
+// function 関数名(引数){
+//   処理
+//   return 戻り値;
+// }
