cb465e297e3cba54eb9726e8f5b23fbee0289a1c
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-10-07 13:26:34 +0900

    [Finish] JavaScript Lesson6-2

---
+++ Lesson6/app2.js
@@ -0,0 +1,13 @@
+// Lesson6-2
+$(function(){
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+})
+
+// $('.セレクタ名').on('click',function() {
+//   イベント発生時に行われる処理
+// });
